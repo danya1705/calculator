f20c0b9f4e4542ced373d79d6b1804e45d5d689a
danya1705 <danya17@gmail.com>
2022-10-21 14:11:15 +0300

    tests hw

--- src/main/java/pro/sky/java/course2/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/java/course2/calculator/service/CalculatorServiceImpl.java
@@ -6,23 +6,23 @@ import org.springframework.stereotype.Service;
 public class CalculatorServiceImpl implements CalculatorService {
 
     @Override
-    public String calculatePlus(int num1, int num2) {
-        return Integer.toString(num1 + num2);
+    public int calculatePlus(int num1, int num2) {
+        return num1 + num2;
     }
 
     @Override
-    public String calculateMinus(int num1, int num2) {
-        return Integer.toString(num1 - num2);
+    public int calculateMinus(int num1, int num2) {
+        return num1 - num2;
     }
 
     @Override
-    public String calculateMultiply(int num1, int num2) {
-        return Integer.toString(num1 * num2);
+    public int calculateMultiply(int num1, int num2) {
+        return num1 * num2;
     }
 
     @Override
-    public String calculateDivide(int num1, int num2) {
-        if (num2 == 0) return "∞";
-        return Integer.toString(num1 / num2);
+    public double calculateDivide(int num1, int num2) {
+        if (num2 == 0) throw new IllegalArgumentException("Division by zero");
+        return (double) num1 / num2;
     }
 }
